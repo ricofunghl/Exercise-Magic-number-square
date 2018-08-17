e2b9451419f1c7ccf42c191220256838652f28a7
ricofunghl <ricofung@Ricos-iMac.local>
2018-08-16 23:25:17 -0600

    code to check if rows are equal

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
 
 $(document).ready(function(){
     var rowCount = 3; // default row count is 3
-    
-    
+    var rowTotalArr;
+    var colTotalArr;
+    var result;
+ 
     //create table with rowCount
     var createTable = function(){
         
-        $(".table").html(" ");
-         //  rowCount = document.querySelector("input[name='rowCount']").value;
-      rowCount = $("input[name='rowCount']").val()
+    $(".table").html(" ");
+//  rowCount = document.querySelector("input[name='rowCount']").value;
+    rowCount = $("input[name='rowCount']").val()
         
-        $(".table").append("<table></table>");
+    $(".table").append("<table></table>");
         
-        for (var i=0; i<rowCount; i++)
+    for (var i=0; i<rowCount; i++)
             {
                 $("table").append("<tr class=row" + i +"></tr>")
 
             }
-      for (var i=0; i<rowCount; i++)
+    for (var i=0; i<rowCount; i++)
             {
 
                 $("tr").append("<td><input type='number' name='number' value=2 class=col" + i + "></td>")
@@ -27,38 +29,53 @@ $(document).ready(function(){
     
     //check numbers
     var checkNumber = function(){
-        var rowTotalArr = [];
-        var colTotalArr = [];
-        var cols = [];
-        var rows = [];
-        var i =0
-        
-        //checking earh row
+        result=true;
+        rowTotalArr = [];
+        colTotalArr = [];
+
+    //storing each row
         for(var ii=0;ii<rowCount;ii++)
-            {
-                var rowTotal = 0;
+        {
+            var rowTotal = 0;
             
-                for (i=0;i<rowCount;i++)
-                    {
-                        rowTotal += parseInt(document.querySelector("tr.row" +ii +" input.col" + i).value);
-                     }            
-                rowTotalArr.push(rowTotal);
-            }
+            for (var i=0;i<rowCount;i++)
+            {
+                rowTotal += parseInt(document.querySelector("tr.row" +ii +" input.col" + i).value);
+            }        
+           
+            rowTotalArr.push(rowTotal);
+       }
     
-         for(var ii=0;ii<rowCount;ii++)
-            {
-                var colTotal = 0;
+    //storing each col
+        for(var ii=0;ii<rowCount;ii++)
+        {
+            var colTotal = 0;
             
-                for (i=0;i<rowCount;i++)
-                    {
-                        colTotal += parseInt(document.querySelector("tr.row" +i +" input.col" + ii).value);
-                     }            
-                colTotalArr.push(colTotal);
+            for (var i=0;i<rowCount;i++)
+                {
+                    colTotal += parseInt(document.querySelector("tr.row" +i +" input.col" + ii).value);
+                }  
+            
+            colTotalArr.push(colTotal);
+        }
+        
+        
+    //checking if sums are equal
+        for (var i=0;i<rowCount-1;i++)
+            {
+                if(rowTotalArr[i] != rowTotalArr[i+1])
+                {
+                    result = false;
+                }
             }
         
-        console.log(colTotalArr);
         
+        alert(result);
+            
+            
     }
+    
+    
     
     
     
